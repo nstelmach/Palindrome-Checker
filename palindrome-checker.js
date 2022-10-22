1ac227e9d9288d2f746dcb511d66605f32720375
nstelmach <90278376+nstelmach@users.noreply.github.com>
2022-10-22 21:34:44 +0200

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,26 +1,22 @@
+function reverseArray(array) {
+  let arrBackward = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    arrBackward.push(array[i]);
+  }
+  return arrBackward;
+}
+
 function palindrome(str) {
-  let newStr = str
-  .replace(/[^a-zA-Z0-9]/g, "")
-  .toLowerCase()
-  .split("");
-  
-  let strForward = [];
-  let strBackward = [];
+  let strArray = str
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase()
+    .split("");
 
-  for (let i = 0; i < newStr.length; i++) {
-    strForward.push(newStr[i]);
-  }
+  let strBackward = reverseArray(strArray);
 
-  for (let i = newStr.length-1; i >= 0; i--) {
-    strBackward.push(newStr[i]);
-  }
-
- for (let i = 0; i < newStr.length; i++) {
-    if (!(strForward[i] === strBackward[i])) {
+  for (let i = 0; i < strArray.length; i++) {
+    if (!(strArray[i] === strBackward[i])) {
       return false;
-    }
-    else {
-      continue;
     }
   }
   return true;
